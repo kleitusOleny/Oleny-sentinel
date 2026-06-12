b0d53abfded3b5c49c78ac6081711e744cb3fc1c
kleitusOleny <23130383@st.hcmuaf.edu.vn>
2026-06-12 21:57:22 +0700

    init: khởi tạo dự án

---
+++ src/test/java/com/server/sentinel/serversentinel/ServerSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.sentinel.serversentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSentinelApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
